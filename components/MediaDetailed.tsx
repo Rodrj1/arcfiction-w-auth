5c46ac6b460c45fe7d774adedf47eaad868f4568
Rodrj1 <rodrigo_mdq93@hotmail.com>
2023-04-25 19:19:47 -0300

    Fixed responsive media in Seasons and Network section

--- components/MediaDetailed.tsx
+++ components/MediaDetailed.tsx
@@ -89,17 +89,17 @@ export default function MediaDetailed({ media }: { media: MediaDetails }) {
       </div>
 
       {isTvShow && (
-        <div className="mt-14 bg-slate-600/10 flex flex-col sm:flex-row justify-evenly items-center h-auto w-full relative p-2 sm:px-16 gap-10">
+        <div className="mt-14 bg-slate-600/10 flex flex-col xl:flex-row justify-evenly items-start h-auto w-full relative p-2 sm:px-16 gap-10">
           <ul>
-            <h2 className="text-xl text-slate-300 font-bold text-center sm:text-left">
+            <h2 className="text-xl text-slate-300 font-bold text-center">
               SEASONS
             </h2>
             {media.seasons?.map((season) => (
               <article
-                className="flex flex-col sm:flex-row my-5 gap-5 justify-center items-center p-3 sm:p-0"
+                className="flex flex-col sm:flex-row my-5 gap-5 justify-center items-center p-3 sm:p-0 w-[100%] md:w-full lg:w-[100%]"
                 key={season.id}
               >
-                <div className={`h-[300px] relative w-[300px]`}>
+                <div className={`xs:h-[280px] xs:w-full sm:h-[300px] relative sm:w-[300px]`}>
                   <Image
                     alt={season.name}
                     fill
@@ -109,25 +109,25 @@ export default function MediaDetailed({ media }: { media: MediaDetails }) {
                   />
                 </div>
 
-                <div>
+                <div className="w-full md:w-[60%] lg:w-[full]">
                   <h2 className="text-lg text-red-600">{season.name}</h2>
                   <h3 className="font-bold text-sm">
                     {season.air_date} Episodes: {season.episode_count}
                   </h3>
-                  <p className="w-full sm:w-[500px]">{season.overview}</p>
+                  <p>{season.overview}</p>
                 </div>
               </article>
             ))}
           </ul>
 
           {media.networks && (
-            <ul>
-              <h2 className="text-xl text-center sm:text-right text-slate-300 font-bold">
+            <ul className="md:flex flex-row flex-wrap gap-4 md:justify-center w-[100%]">
+              <h2 className="text-xl text-center text-slate-300 font-bold w-full">
                 NETWORKS
               </h2>
               {media.networks.map((network) => (
                 <article
-                  className="flex my-5 gap-5 items-center justify-between sm:justify-end"
+                  className="flex my-5 gap-5 items-center justify-between sm:justify-end md:flex-col lg:flex-row"
                   key={network.id}
                 >
                   <div className={`h-[100px] relative w-[100px]`}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xs: '280px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
-        customgray: "#1a171e"
-      }
+        customgray: '#1a171e',
+      },
     },
   },
   plugins: [],
-}
+};
